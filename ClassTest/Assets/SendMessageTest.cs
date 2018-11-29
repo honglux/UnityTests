5ce0cb515bdc6b3a4fc1b53b73146ffe75ff14c1
honglux <xuhlusa@gmail.com>
2018-11-28 22:13:45 -0500

    send message test

---
+++ ClassTest/Assets/SendMessageTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessageTest : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        SendMessage("send_message_test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ClassTest/Assets/SendMessageTest2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessageTest2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void send_message_test()
+    {
+        Debug.Log("send_message_test");
+    }
+}
