043a2262610d5ce7fff36e7cf1a013f0f90c2ca6
honglux <xuhlusa@gmail.com>
2018-11-30 11:26:42 -0500

    VR physics test

---
+++ PhysicsTest/Assets/RightController_Grab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightController_Grab : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        transform.localPosition =
+                OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.localRotation =
+                        OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+    }
+}
