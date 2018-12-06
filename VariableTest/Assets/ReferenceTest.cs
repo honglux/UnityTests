0089fdc5b28a8ffa305b1fd2ad3e410098f4e370
honglux <xuhlusa@gmail.com>
2018-12-05 21:50:39 -0500

    VariableTest

---
+++ VariableTest/Assets/ReferenceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceTest : MonoBehaviour {
+
+    private Color color;
+
+	// Use this for initialization
+	void Start () {
+        color = GetComponent<MeshRenderer>().material.color;
+
+        color = Color.red;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
