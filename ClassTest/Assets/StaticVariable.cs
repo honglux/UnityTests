745d071d4016c62cc10773c57109b34ba41605ac
honglux <xuhlusa@gmail.com>
2019-07-04 22:17:43 -0400

    MT accord

---
+++ ClassTest/Assets/StaticVariable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVariable
+{
+
+    private int a = 0;
+    public static int ref_counter = 0;
+
+    public StaticVariable()
+    {
+        ref_counter++;
+    }
+
+}
---
+++ ClassTest/Assets/StaticVariableCall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVariableCall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StaticVariable a = new StaticVariable();
+        StaticVariable b = new StaticVariable();
+        StaticVariable c = new StaticVariable();
+        StaticVariable d = new StaticVariable();
+
+        Debug.Log("StaticVariable " + StaticVariable.ref_counter);
+        //Debug.Log("d " + d.ref_counter);  //Wrong;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
