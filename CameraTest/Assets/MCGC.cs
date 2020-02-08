094d5698afcb522ffb7d3cc351aeb43f220fd2c4
honglux <xuhlusa@gmail.com>
2020-02-08 16:49:16 -0500

    CT

---
+++ CameraTest/Assets/MCGC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCGC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Display.displays.Length == 2)
+        {
+            Display.displays[1].Activate();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
