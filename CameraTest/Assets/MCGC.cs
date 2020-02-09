46f934949724c8b364a22cc91c28589a451311ad
honglux <xuhlusa@gmail.com>
2020-02-08 21:42:09 -0500

    CT

--- CameraTest/Assets/MCGC.cs
+++ CameraTest/Assets/MCGC.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MCGC : MonoBehaviour
 {
+    [SerializeField] private Camera camera1;
+    [SerializeField] private Camera camera2;
+    [SerializeField] private GameObject acuity_OBJ;
+    [SerializeField] private GameObject target_OBJ;
+    [SerializeField] private Canvas canvas1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +17,16 @@ public class MCGC : MonoBehaviour
         {
             Display.displays[1].Activate();
         }
+
+        camera1.transform.eulerAngles = new Vector3(0.0f, 318.0f, 0.0f);
+        //Vector3 screenPos = camera1.WorldToViewportPoint(target_OBJ.transform.position);
+        //screenPos.x *= canvas1.GetComponent<RectTransform>().rect.width;
+        //screenPos.x -= canvas1.GetComponent<RectTransform>().rect.width / 2.0f;
+        //screenPos.y *= canvas1.GetComponent<RectTransform>().rect.height;
+        //screenPos.y -= canvas1.GetComponent<RectTransform>().rect.height / 2.0f;
+        //screenPos.z = 0.0f;
+        //Debug.Log("pos "+ screenPos.ToString("F2"));
+        //acuity_OBJ.GetComponent<RectTransform>().localPosition = screenPos;
     }
 
     // Update is called once per frame
